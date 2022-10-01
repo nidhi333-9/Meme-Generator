bf88d23fed76c530a671554f2e8fbce3b180d6e9
nidhikumari <kumari9936nidhi@gmail.com>
2022-10-02 00:40:00 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 
 import './App.css'
-import Form from './components/Form'
+// import Form from './components/Form'
 import Main from './components/Main'
 import Navbar from './components/Navbar'
-import Sign from './components/Sign'
+// import Sign from './components/Sign'
 
 
 function App() {
 
   return (
     <div className="App">
-      {/* <Navbar />
-      <Main /> */}
+      <Navbar />
+      <Main />
+
+
       {/* <Form /> */}
-      <Sign />
+      {/* <Sign /> */}
     </div>
   )
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import MemeData from "../MemeData";
-
+import "../App.css"
 function Main() {
     const [meme, setMeme] = useState({
         topText: "",
@@ -8,15 +8,29 @@ function Main() {
         randomImage: "http://i.imgflip.com/1bij.jpg"
     })
 
-    const [allMemeImages, setAllMemeImages] = useState(MemeData)
+    const [allMemeImages, setAllMemeImages] = useState([])
     // const [memeImage, setMemeImage] = useState("")
     function getMeme() {
-        const memeArray = allMemeImages.data.memes
-        const randomNumber = Math.floor(Math.random() * memeArray.length);
-        const url = memeArray[randomNumber].url
+
+        const randomNumber = Math.floor(Math.random() * allMemeImages.length);
+        const url = allMemeImages[randomNumber].url
         setMeme(prevMeme => ({
             ...prevMeme,
             randomImage: url
+        }))
+    }
+
+    useEffect(() => {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res => res.json())
+            .then(data => setAllMemeImages(data.data.memes))
+    }, [])
+
+    function handleChange(event) {
+        const { name, value } = event.target
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
         }))
     }
     return (
@@ -24,16 +38,30 @@ function Main() {
             <div className="container border p-5">
                 <div className="input  sm:flex sm:justify-between ">
                     <div className="">
-                        <input className="border w-2/3 sm:w-auto p-2 my-3 rounded-md" type="text" placeholder="shup up" />
+                        <input
+                            className="border w-2/3 sm:w-auto p-2 my-3 rounded-md"
+                            type="text"
+                            placeholder="shup up"
+                            onChange={handleChange}
+                            name="topText"
+                            value={meme.topText} />
                     </div>
                     <div>
-                        <input className="border w-2/3 sm:w-auto p-2 my-3 rounded-md" type="text" placeholder="and take the money" />
+                        <input
+                            className="border w-2/3 sm:w-auto p-2 my-3 rounded-md"
+                            type="text"
+                            placeholder="and take the money"
+                            onChange={handleChange}
+                            name="bottomText"
+                            value={meme.bottomText} />
                     </div>
                 </div>
 
                 <button onClick={getMeme} className="bg-violet-800 w-3/4 my-5 sm:w-full p-2 text-white rounded-md font-semibold">Get A New Meme Image 🖼️</button>
-                <div className="w-fit">
-                    <img src={meme.randomImage} alt="" />
+                <div className="meme">
+                    <img className="meme-image" src={meme.randomImage} alt="" />
+                    <h2 className="meme--text top">{meme.topText}</h2>
+                    <h2 className="meme--text bottom">{meme.bottomText}</h2>
                 </div>
             </div>
         </>
